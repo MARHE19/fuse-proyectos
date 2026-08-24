0166862415b605545b87a561cd41c30540826c72
Marhe <hermysalcedo2@gmail.com>
2026-08-23 23:36:39 -0500

    Agregar sitemap, robots.txt y mejoras de SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,12 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://fuse-proyectos.vercel.app', // luego lo cambias por tu dominio propio
+  integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()]
   }
